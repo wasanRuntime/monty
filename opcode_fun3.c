1bdac82f68d25064dd525fa2f404353bf4069d61
wasanRuntime <86593502+wasanRuntime@users.noreply.github.com>
2023-08-24 19:51:11 +0300

    Update opcode_fun3.c
    func3

--- opcode_fun3.c
+++ opcode_fun3.c
@@ -71,13 +71,13 @@ void op_mul(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * op_mod - mod two elements on the top of the stack
+ * op_sub - sub two elements on the top of the stack
  * @stack: pointer to the head of the stack
  * @line_number: number of the current line
  *
  * Return: the number of nodes
  */
-void op_mod(stack_t **stack, unsigned int line_number)
+void op_sub(stack_t **stack, unsigned int line_number)
 {
 	size_t n = 0;
 	stack_t *temp = *stack;
@@ -89,21 +89,13 @@ void op_mod(stack_t **stack, unsigned int line_number)
 		handle_dlist_head(var.stack_head);
 		free(var.getl_info);
 		fclose(var.fp_struct);
-		fprintf(stderr, "L%u: can't mod, stack too short\n", var.n_lines);
-		exit(EXIT_FAILURE);
-	}
-	if ((*stack)->next->n == 0)
-	{
-		handle_dlist_head(var.stack_head);
-		free(var.getl_info);
-		fclose(var.fp_struct);
-		fprintf(stderr, "L%u: division by zero\n", var.n_lines);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", var.n_lines);
 		exit(EXIT_FAILURE);
 	}
 	if (*stack != NULL)
 	{
 		*stack = (*stack)->next;
-		(*stack)->n = (*stack)->n % (*stack)->prev->n;
+		(*stack)->n = (*stack)->n - (*stack)->prev->n;
 		(*stack)->prev = NULL;
 		free(temp);
 	}
