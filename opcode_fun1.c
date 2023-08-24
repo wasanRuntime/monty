c004590761d2f330e9b7e8575a9e8e4ad34c2d2e
wasanRuntime <86593502+wasanRuntime@users.noreply.github.com>
2023-08-24 19:47:03 +0300

    Update opcode_fun1.c
    updated

--- opcode_fun1.c
+++ opcode_fun1.c
@@ -33,6 +33,31 @@ void op_push(stack_t **stack, unsigned int line_number)
 }
 
 /**
+ * op_swap - swaps the top two elements of the stack.
+ * @stack: pointer to the head of the stack
+ * @line_number: number of the current line
+ *
+ * Return: void.
+ */
+void op_swap(stack_t **stack, unsigned int line_number)
+{
+	int tmp = 0;
+
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		free(var.getl_info);
+		fclose(var.fp_struct);
+		handle_dlist_head((*stack));
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
+}
+
+/**
  * op_pall - prints all the values on the stack
  * @stack: pointer to the head of the stack
  * @line_number: number of the current line
@@ -72,29 +97,4 @@ void op_pint(stack_t **stack, unsigned int line_number)
 	}
 
 	printf("%d\n", ((*stack))->n);
-}
-
-/**
- * op_swap - swaps the top two elements of the stack.
- * @stack: pointer to the head of the stack
- * @line_number: number of the current line
- *
- * Return: void.
- */
-void op_swap(stack_t **stack, unsigned int line_number)
-{
-	int tmp = 0;
-
-	if ((*stack) == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		free(var.getl_info);
-		fclose(var.fp_struct);
-		handle_dlist_head((*stack));
-		exit(EXIT_FAILURE);
-	}
-
-	tmp = (*stack)->n;
-	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = tmp;
 }
